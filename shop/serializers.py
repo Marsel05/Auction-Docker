6747237659857e57dbb4b56bb2eada22d9fe5b0d
asadulladev <usmanbeliever@gmail.com>
2024-05-28 13:41:52 +0600

    fdfdf

---
+++ shop/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from .models import UserProfile, Category, CarMake, Model, Car, Bet, Comment
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class CarMakeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarMake
+        fields = '__all__'
+
+
+class ModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Model
+        fields = '__all__'
+
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ['id', 'car_name', 'category', 'car_make', 'model', 'description', 'year', 'price', 'add_date',
+                  'city', 'country', 'mileage', 'with_photo', 'drive', 'engine', 'volume']
+
+
+class BetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bet
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
